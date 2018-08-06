09d2f83d021e9fb84a792cb2a75f4979039bc130
Matias Meno <m@tias.me>
2018-08-06 18:18:15 +0200

    Use shorter comment lines

--- lib/src/resources/messages.dart
+++ lib/src/resources/messages.dart
@@ -5,16 +5,18 @@ class Messages {
 
   Messages(this.client);
 
-  /// - `sendAsync`: enable a background sending mode that is optimized for bulk sending. In async mode, messages/send
-  ///   will immediately return a status of "queued" for every recipient. To handle rejections when sending in async
-  ///   mode, set up a webhook for the 'reject' event. Defaults to false for messages with no more than 10 recipients;
-  ///   messages with more than 10 recipients are always sent asynchronously, regardless of the value of async.
-  /// - `ipPool`: the name of the dedicated ip pool that should be used to send the message. If you do not have any
-  ///   dedicated IPs, this parameter has no effect. If you specify a pool that does not exist, your default pool will
-  ///   be used instead.
-  /// - `sendAt`: when this message should be sent. If you specify a time in the past, the message will be sent
-  ///   immediately. An additional fee applies for scheduled email, and this feature is only available to accounts with
-  ///   a positive balance.
+  /// - `sendAsync`: enable a background sending mode that is optimized for bulk sending.
+  ///   In async mode, messages/send will immediately return a status of "queued" for
+  ///   every recipient. To handle rejections when sending in async mode, set up a webhook
+  ///   for the 'reject' event. Defaults to false for messages with no more than 10
+  ///   recipients; messages with more than 10 recipients are always sent asynchronously,
+  ///   regardless of the value of async.
+  /// - `ipPool`: the name of the dedicated ip pool that should be used to send the
+  ///   message. If you do not have any dedicated IPs, this parameter has no effect. If
+  ///   you specify a pool that does not exist, your default pool will be used instead.
+  /// - `sendAt`: when this message should be sent. If you specify a time in the past,
+  ///   the message will be sent immediately. An additional fee applies for scheduled
+  ///   email, and this feature is only available to accounts with a positive balance.
   Future<SentMessagesResponse> send(
     OutgoingMessage message, {
     bool sendAsync: false,
@@ -31,10 +33,11 @@ class Messages {
     return await client.call('messages/send', body, new SentMessagesResponse());
   }
 
-  /// - `templateName`: the immutable name or slug of a template that exists in the user's account.
-  ///   For backwards-compatibility, the template name may also be used but the immutable slug is preferred.
-  /// - `templateContent`: a Map where the key is the content block to set the content for, and the value is the
-  ///   actual content to put into the block.
+  /// - `templateName`: the immutable name or slug of a template that exists in the
+  ///   user's account. For backwards-compatibility, the template name may also be used
+  ///   but the immutable slug is preferred.
+  /// - `templateContent`: a Map where the key is the content block to set the content
+  ///   for, and the value is the actual content to put into the block.
   ///
   /// For `sendAsync`, `ipPool` and `sendAt` please look at the [send] documentation.
   Future<SentMessagesResponse> sendTemplate(
